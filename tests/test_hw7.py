c4eecc12c68b05dea0f84fb415e4625251a3d7f2
Reina Krumvieda <reina.krumvieda@colorado.edu>
2022-10-27 19:52:12 +0000

    fixing functional and unit tests

--- tests/test_hw7.py
+++ tests/test_hw7.py
@@ -10,7 +10,7 @@ import sys
 import numpy as np
 
 sys.path.append('../')
-import data_processor
+import data_processor  # nopep8
 
 
 class TestHw7(unittest.TestCase):
@@ -23,11 +23,11 @@ class TestHw7(unittest.TestCase):
         cls.rand_row_val = random.randint(1, 10)
         # generate random valid column number
         cls.rand_col_val = random.randint(1, 10)
-        #set up file name
+        # set up file name
         cls.file_name = '../iris.data'
         # test file name
         cls.test_file_name = 'test_file_name'
-        #random matrix
+        # random matrix
         cls.none_mat = None
 
     @classmethod
@@ -41,65 +41,67 @@ class TestHw7(unittest.TestCase):
     def test_get_random_matrix(self):
 
         # 1. positive assertions with randomness
-        self.assertEqual(data_processor.get_random_matrix(self.rand_bad_val,
-                                             self.rand_col_val), None)
+        self.assertEqual(
+            data_processor.get_random_matrix(
+                self.rand_bad_val, self.rand_col_val), None)
 
         # 2. negative assertion with randomness
         rand_mat = np.random.uniform(
-            0,1,size=(self.rand_row_val,self.rand_col_val) )
+            0, 1, size=(self.rand_row_val, self.rand_col_val))
 
         self.assertNotEqual(data_processor.get_random_matrix(
             self.rand_row_val, self.rand_col_val).any(), None)
 
         # 3. Error Case (return None)
         try:
-            assert data_processor.get_random_matrix(self.rand_bad_val,
-                                       self.rand_bad_val) != None,\
-                                        "ERROR1: Used Neg Inputs!"
-            self.assertEqual(data_processor.get_random_matrix(self.rand_bad_val,
-                                                 self.rand_bad_val), None)
+            assert data_processor.get_random_matrix(
+                self.rand_bad_val, self.rand_bad_val) is not None,\
+                "ERROR1: Used Neg Inputs!"
+            self.assertEqual(data_processor.get_random_matrix(
+                self.rand_bad_val, self.rand_bad_val), None)
         except AssertionError as msg:
             print(msg)
 
     def test_get_file_dimensions(self):
+        # 1. positive assertion
+        self.assertEqual(data_processor.get_file_dimensions(self.file_name),
+                         (150, 5))
 
-            # 1. positive assertion
-            self.assertEqual(data_processor.get_file_dimensions(self.file_name),
-                             (150, 5))
+        # 2. negative assertion
+        self.assertNotEqual(data_processor.get_file_dimensions(self.file_name),
+                            (0, 0))
 
-            # 2. negative assertion
-            self.assertNotEqual(data_processor.get_file_dimensions(self.file_name),
-                                (0, 0))
-
-            # 3. Error Case (return (0,0))
-            garbage_file_name = "sudflh.data"
-            try:
-                assert data_processor.get_file_dimensions(
-                    garbage_file_name) != (0,0),"ERROR2: Filename DNE!"
-                self.assertEqual(data_processor.get_file_dimensions
-                                 (self.file_name),(0,0))
-            except AssertionError as msg:
-                print(msg)
+        # 3. Error Case (return (0,0))
+        garbage_file_name = "sudflh.data"
+        try:
+            assert data_processor.get_file_dimensions(
+                garbage_file_name) != (0, 0), "ERROR2: Filename DNE!"
+            self.assertEqual(data_processor.get_file_dimensions
+                             (self.file_name), (0, 0))
+        except AssertionError as msg:
+            print(msg)
 
     def test_write_matrix_to_file(self):
 
         # 1. positive assertion
         self.assertEqual(data_processor.write_matrix_to_file(
-            self.rand_row_val, self.rand_col_val,self.test_file_name), 1)
+            self.rand_row_val, self.rand_col_val, self.test_file_name), 1)
 
         # 2. negative assertion
         self.assertNotEqual(data_processor.write_matrix_to_file(
-            self.rand_row_val, self.rand_col_val,self.test_file_name), None)
+            self.rand_row_val, self.rand_col_val, self.test_file_name), None)
 
         # 3. Error Case (return None -- used negative inputs)
         try:
             assert data_processor.write_matrix_to_file(
-                self.rand_bad_val, self.rand_bad_val,self.test_file_name) != None,\
-                    "ERROR3: Used Neg Inputs!"
+                self.rand_bad_val, self.rand_bad_val,
+                self.test_file_name) is not None, "ERROR3: Used Neg Inputs!"
             self.assertEqual(data_processor.write_matrix_to_file(
-                self.rand_bad_val, self.rand_bad_val,self.test_file_name), None)
+                self.rand_bad_val, self.rand_bad_val,
+                self.test_file_name), None)
         except AssertionError as msg:
             print(msg)
+
 
 if __name__ == '__main__':
     unittest.main()
