a65798081132cf5f0bebcb80e568739fb85fc2c0
Reina Krumvieda <63476614+reina-krumvieda@users.noreply.github.com>
2022-10-27 15:58:45 -0600

    Update test_hw7.sh
    updated to pass actions functional tests

--- tests/test_hw7.sh
+++ tests/test_hw7.sh
@@ -6,21 +6,21 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run get_random_matrix python ../data_processor.py get_random_matrix 2 2 
+run get_random_matrix python data_processor.py get_random_matrix 2 2 
 # functional test for exit codes
 assert_exit_code 0
 # functional test for returning matrix
 # expecting random float array (not (0,0))
 assert_in_stdout [0.
 
-run get_file_dimensions python ../data_processor.py get_file_dimensions
+run get_file_dimensions python data_processor.py get_file_dimensions
 # functional test for exit codes
 assert_exit_code 0
 # functional test for returning file dimensions of iris.data
 # expecting dimensions of (150, 5)
 assert_in_stdout 150
 
-run write_matrix_to_file python ../data_processor.py write_matrix_to_file
+run write_matrix_to_file python data_processor.py write_matrix_to_file
 # functional test for exit codes
 assert_exit_code 0
 # functional test for returning success value
